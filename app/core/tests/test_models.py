a05c47f97d98486f2ec5dc7a11d67a9311864aa7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-28 22:32:13 +0530

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,6 +26,7 @@ class ModelTests(TestCase):
             ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
+
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
